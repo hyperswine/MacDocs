ada53bca8e92da1da18064628f76946475aa4754
Jasen Qin <jiasheng.qin@student.unsw.edu.au>
2023-04-11 00:12:40 +1000

    Add swift

---
+++ MacDocs/MacDocsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacDocsApp.swift
+//  MacDocs
+//
+//  Created by Jason Qin on 8/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct MacDocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
